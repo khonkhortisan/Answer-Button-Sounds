13ae7c32c4c85756d35118b631ec87686790c515
Kyle Mills <Khonkhortisan@gmail.com>
2020-02-20 07:09:21 -0800

    drop 2.1.19 redux

--- __init__.py
+++ __init__.py
@@ -12,9 +12,9 @@ addon_path = os.path.dirname(__file__)
 user_files = os.path.join(addon_path, "user_files")
 
 def answersound(card, ease, early):
-	clearAudioQueue() #force feedback to play now
 	#add sounds for extra buttons here
 	if ease == 1:
+		clearAudioQueue() #force feedback to play now. Apparently this has to be within the if to be defined. Or I have to stop using the python console as if it's the addon.
 		play(os.path.join(user_files, "again.mp3"))
 	if ease == 2:
 		clearAudioQueue()
@@ -29,16 +29,15 @@ def answersound(card, ease, early):
 	#preventclearingAudioQueue() #see ~~nextCard()~~ play_tags #force feedback to continue playing now
 hooks.schedv2_did_answer_review_card.append(answersound)
 
-if LetSoundBleedOntoNextCard_InsteadOf_CancelingSoundEffectSometimes:
-    def _play_tags(self, tags: List[AVTag]) -> None:
-        """Clear the existing queue, then start playing provided tags."""
-        self._enqueued = tags[:]
-        #if self.interrupt_current_audio:
-        #if self.interrupt_current_audio and not nextCard
-        if self.interrupt_current_audio and False: #TODO: do clear audio when flipping to back, don't clear it when going to the next card. This was easier when it was in the nextCard function so I could just disable it there.
-            self._stop_if_playing()
-        self._play_next_if_idle()
-    AVPlayer.play_tags=_play_tags
+def _play_tags(self, tags):
+    """Clear the existing queue, then start playing provided tags."""
+    self._enqueued = tags[:]
+    #if self.interrupt_current_audio:
+    #if self.interrupt_current_audio and not nextCard
+    if self.interrupt_current_audio and False: #TODO: do clear audio when flipping to back, don't clear it when going to the next card. This was easier when it was in the nextCard function so I could just disable it there.
+        self._stop_if_playing()
+    self._play_next_if_idle()
+AVPlayer.play_tags=_play_tags
 
 #def pageflip(self, url):
 #	play(os.path.join(user_files, "flip.mp3"))
