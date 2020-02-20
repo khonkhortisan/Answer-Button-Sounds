383abafad06ab96ecfdde543e278ae952e639d80
Kyle Mills <Khonkhortisan@gmail.com>
2020-02-20 06:45:07 -0800

    Disable Tools→Preferences→Basic→☑ Interrupt current audio when answering drop 2.1.19 Side effect: front side audio doesn't stop playing on back side

--- __init__.py
+++ __init__.py
@@ -1,28 +1,44 @@
 """
-Answer Button Sounds addon for Anki 2.1 by Kyle "Khonkhortisan" Mills
+Answer Button Sounds addon for Anki 2.1.20 by Kyle "Khonkhortisan" Mills
 """
 
 import os
 
-from aqt.reviewer import Reviewer
 from anki import hooks
-from anki.sound import play, clearAudioQueue
-
-#don't let nextCard crash (I wish it would tell me when I'm missing includes)
-#from aqt.utils import (askUserDialog, downArrow, mungeQA,
-#                       qtMenuShortcutWorkaround, tooltip)
-from aqt.utils import askUserDialog
-from aqt.qt import * #QMessageBox
-
-
-#Toggle this variable if the nextCard function conflicts with another addon,
-#or you don't like the next card's audio delay
-#and are willing to lose this addon's sound effects some of the time.
-LetSoundBleedOntoNextCard_InsteadOf_CancelingSoundEffectSometimes=True
-
+from aqt.sound import play, clearAudioQueue, AVPlayer
+#from aqt.reviewer import Reviewer
 
 addon_path = os.path.dirname(__file__)
 user_files = os.path.join(addon_path, "user_files")
+
+def answersound(card, ease, early):
+	clearAudioQueue() #force feedback to play now
+	#add sounds for extra buttons here
+	if ease == 1:
+		play(os.path.join(user_files, "again.mp3"))
+	if ease == 2:
+		clearAudioQueue()
+		play(os.path.join(user_files, "hard.mp3"))
+	if ease == 3:
+		clearAudioQueue()
+		play(os.path.join(user_files, "good.mp3"))
+	if ease == 4:
+		clearAudioQueue()
+		play(os.path.join(user_files, "easy.mp3"))
+	#add sounds for extra buttons here
+	#preventclearingAudioQueue() #see ~~nextCard()~~ play_tags #force feedback to continue playing now
+hooks.schedv2_did_answer_review_card.append(answersound)
+
+if LetSoundBleedOntoNextCard_InsteadOf_CancelingSoundEffectSometimes:
+    def _play_tags(self, tags: List[AVTag]) -> None:
+        """Clear the existing queue, then start playing provided tags."""
+        self._enqueued = tags[:]
+        #if self.interrupt_current_audio:
+        #if self.interrupt_current_audio and not nextCard
+        if self.interrupt_current_audio and False: #TODO: do clear audio when flipping to back, don't clear it when going to the next card. This was easier when it was in the nextCard function so I could just disable it there.
+            self._stop_if_playing()
+        self._play_next_if_idle()
+    AVPlayer.play_tags=_play_tags
 
 #def pageflip(self, url):
 #	play(os.path.join(user_files, "flip.mp3"))
@@ -38,73 +54,3 @@ user_files = os.path.join(addon_path, "user_files")
 #	#	#preventclearingAudioQueue()
 #	#	error();
 #Reviewer._linkHandler = hooks.wrap(Reviewer._linkHandler, pageflip, "before")
-
-
-#Reviewer._answerCard = hooks.wrap(Reviewer._answerCard, function(self, ease) {
-#This isn't haskell, have to have different names for different numbers of arguments
-def answersound3(card, ease, early): #2.1.20+
-	answersound1(ease)
-def answersound2(self, ease): #2.1.19-
-	answersound1(ease)
-def answersound1(ease):
-	#add sounds for extra buttons here
-	if ease == 1:
-		clearAudioQueue() #force feedback to play
-		play(os.path.join(user_files, "again.mp3"))
-		#preventclearingAudioQueue() #see nextCard()
-	if ease == 2:
-		clearAudioQueue()
-		play(os.path.join(user_files, "hard.mp3"))
-		#preventclearingAudioQueue()
-	if ease == 3:
-		clearAudioQueue()
-		play(os.path.join(user_files, "good.mp3"))
-		#preventclearingAudioQueue()
-	if ease == 4:
-		clearAudioQueue()
-		play(os.path.join(user_files, "easy.mp3"))
-		#preventclearingAudioQueue()
-	#add sounds for extra buttons here
-#}, "before")
-try:	hooks.schedv2_did_answer_review_card.append(answersound3)	#2.1.20+
-except AttributeError:	Reviewer._answerCard = hooks.wrap(Reviewer._answerCard, answersound2, "before")	#2.1.19-
-
-if LetSoundBleedOntoNextCard_InsteadOf_CancelingSoundEffectSometimes:
-	#already cleared the audio queue, don't do it again and lose the sound effects.
-	#but it delays the next card's sounds.
-	#def Reviewer.nextCard(self):
-	def nextCard(self):
-		elapsed = self.mw.col.timeboxReached()
-		if elapsed:
-			part1 = ngettext("%d card studied in", "%d cards studied in", elapsed[1]) % elapsed[1]
-			mins = int(round(elapsed[0]/60))
-			part2 = ngettext("%s minute.", "%s minutes.", mins) % mins
-			fin = _("Finish")
-			diag = askUserDialog("%s %s" % (part1, part2),
-							[_("Continue"), fin])
-			diag.setIcon(QMessageBox.Information)
-			if diag.run() == fin:
-				return self.mw.moveToState("deckBrowser")
-			self.mw.col.startTimebox()
-		if self.cardQueue:
-			# undone/edited cards to show
-			c = self.cardQueue.pop()
-			c.startTimer()
-			self.hadCardQueue = True
-		else:
-			if self.hadCardQueue:
-				# the undone/edited cards may be sitting in the regular queue;
-				# need to reset
-				self.mw.col.reset()
-				self.hadCardQueue = False
-			c = self.mw.col.sched.getCard()
-		self.card = c
-	#	clearAudioQueue()
-		if not c:
-			self.mw.moveToState("overview")
-			return
-		if self._reps is None or self._reps % 100 == 0:
-			# we recycle the webview periodically so webkit can free memory
-			self._initWeb()
-		self._showQuestion()
-	Reviewer.nextCard=nextCard
